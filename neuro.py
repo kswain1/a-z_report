712e9a0e021cd808bd227b38736b540283ecfc0d
kehlinswain <kehlinswain@gmail.com>
2018-10-16 18:25:34 -0500

    key risk area medial and lateral gastrocnemius

---
+++ neuro.py
@@ -0,0 +1,18 @@
+import flask
+
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+
+class Neuro():
+
+	def get_assessment():
+		nuero = html.Div([
+			html.H1("hello"),
+
+			])
+
+		return nuero
